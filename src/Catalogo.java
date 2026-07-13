4afc10a038fe6d592c4349bf24fae7cdc3095452
josue <josue2017ivan@gmail.com>
2026-07-12 19:43:12 -0600

    agregue los metodos y constructores de cada clase

--- src/Catalogo.java
+++ src/Catalogo.java
@@ -1,2 +1,7 @@
+import java.util.ArrayList;
+
 public class Catalogo {
+        public static ArrayList<Alumno> alumnos = new ArrayList<>();
+        public static ArrayList<Grupo> grupos = new ArrayList<>();
+        public static ArrayList<Materia> materias = new ArrayList<>();
 }
